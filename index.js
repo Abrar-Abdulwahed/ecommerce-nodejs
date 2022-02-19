f540266a05e14a828ed966f0484d07fdee1eacc2
A-Abd <abrar.abdulwahed@gmail.com>
2022-02-19 17:07:51 +0300

    install required libs and modules

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
+
 const express = require("express");
+const fetch =  require("node-fetch");
 const app = express();
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=>{
-    console.log(`Listening on ${port}`)
-});
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.set('views', 'views');
 
-app.get('/', (req, res)=>{
-    res.send("hello");
+app.listen(port, console.log(`Listening on ${port}`));
+
+
+app.get(["/", "home"], (req, res) => {
+    fetch('https://dummyjson.com/products/')
+    .then(res => res.json())
+    .then(res.render('index'), {products: res.json()});
+})
+
+
+
+app.get("/favorite", (req, res) => {
+    res.render('favorite');
+})
+
+app.get("/cart", (req, res) => {
+    res.render('cart');
+})
+
+app.get("/contact", (req, res) => {
+    res.render('contact');
 })
